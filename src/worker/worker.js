4ab9d99f885b60cc5b0d9cfa6429efee4593c928
Paul Boutes <paul.boutes@elastic.co>
2019-02-05 01:35:13 +0100

    Compute timing of the parsing request

--- src/worker/worker.js
+++ src/worker/worker.js
@@ -3,6 +3,9 @@ importScripts('./jq.js');
 onmessage = async (event) => {
     const {json, query} = event.data;
     // Interact with our web assembly module
+    const t0 = performance.now();
     const res = await jq.json(json, query);
+    const t1 = performance.now();
+    console.log(`Process jq parsing in ${t1 - t0} ms`);
     postMessage(res);
 }
