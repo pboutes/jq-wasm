da1b79fc5a11e16ca81e0e71cfbd7d08977fd22f
Paul Boutes <paul.boutes@elastic.co>
2019-02-06 02:31:20 +0100

    Add lot of examples, and make fancy UI :fire:

--- src/worker/worker.js
+++ src/worker/worker.js
@@ -3,9 +3,10 @@ importScripts('./jq.js');
 onmessage = async (event) => {
     const {json, query} = event.data;
     // Interact with our web assembly module
-    const t0 = performance.now();
-    const res = await jq.json(json, query);
-    const t1 = performance.now();
-    console.log(`Process jq parsing in ${t1 - t0} ms`);
-    postMessage(res);
+    try {
+        const result = await jq.json(json, query);
+        postMessage(result);
+    } catch (e) {
+        postMessage({error: e.message});
+    }
 }
